e28dd702590f007286750c437f2320310feddc36
git <florah@gmail.com>
2024-10-20 11:07:49 +0300

    add get item functionality

--- index.js
+++ index.js
@@ -52,36 +52,78 @@ tracker
     console.log(`Item '${title}' added successfully`);
   });
 
+//delete item
 
-  //delete item
-
-  tracker
+tracker
   .command("delete")
   .description("delete an item")
   .option("-t, --title <value>", "title of the budget item")
   .action(function (options) {
     const title = options.title;
-    const loadedItems=fs.readFileSync("./data/Items.json", 'utf8');
-    const items= JSON.parse(loadedItems);
-    if (items.length === 0){
-        console.log('no item to delete');
-        return;
+    const loadedItems = fs.readFileSync("./data/Items.json", "utf8");
+    const items = JSON.parse(loadedItems);
+    if (items.length === 0) {
+      console.log("no item to delete");
+      return;
     }
-    const remainingItems=items.filter((currentItem) => currentItem.title !==title);
-    if (remainingItems.length === items.length){
-        console.log(`item '${title}' does not exist`)
-        return;
+    const remainingItems = items.filter(
+      (currentItem) => currentItem.title !== title,
+    );
+    if (remainingItems.length === items.length) {
+      console.log(`item '${title}' does not exist`);
+      return;
     }
-    fs.writeFileSync("./data/Items.json" , JSON.stringify(remainingItems));
-    console.log(`item '${title}'deleted successfully`)
-    
+    fs.writeFileSync("./data/Items.json", JSON.stringify(remainingItems));
+    console.log(`item '${title}'deleted successfully`);
   });
 
+//GET ITEMS
+
+tracker
+  .command("all")
+  .description("gets all items")
+  .option("-t, --title <value>", "title of the budget item")
+  .action(function () {
+    const loadedItems = fs.readFileSync("./data/Items.json", "utf8");
+    const items = JSON.parse(loadedItems);
+
+    if (items.length === 0) {
+      console.log("you dont have any item");
+      return;
+    }
+
+    items.forEach((currentItem) => {
+      console.log(currentItem.title);
+      console.log(currentItem.quantity);
+      console.log(currentItem.price);
+    });
+  });
 
   //GET ITEM
-  
 
+  tracker
+  .command("get")
+  .description("gets a specific budget item by title")
+  .option("-t, --title <value>", "title of the budget item")
+  .action(function (options) {
+    const loadedItems = fs.readFileSync("./data/Items.json", "utf8");
+    const items = JSON.parse(loadedItems);
 
+   
+    if (items.length === 0) {
+      console.log("You don't have any items.");
+      return;
+    }
+    const foundItem = items.find(item => item.title === options.title);
+    if (!foundItem) {
+      console.log(`Item with title "${options.title}" not found.`);
+    } else {
+     
+      console.log(`Title: ${foundItem.title}`);
+      console.log(`Quantity: ${foundItem.quantity}`);
+      console.log(`Price: ${foundItem.price}`);
+    }
+  });
 
 
 tracker.parse(process.argv);
