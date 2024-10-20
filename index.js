c6520ea478db0dbbb4fa0acd22416a6a39abe975
git <florah@gmail.com>
2024-10-20 11:16:33 +0300

    add update functionality

--- index.js
+++ index.js
@@ -16,40 +16,24 @@ tracker
   .description("add an item")
   .option("-t, --title <value>", "title of the budget item")
   .option("-q, --quantity <value>", "quantity of the item")
-  .option("-p, --price <value>", "the price of the item")
+  .option("-p, --price <value>", "price of the item")
   .action(function (options) {
-    const title = options.title;
-    const quantity = options.quantity;
-    const price = options.price;
-
-    const newItem = {
-      title: title,
-      quantity: quantity,
-      price: price,
-    };
-
+    const { title, quantity, price } = options;
     let items = [];
-
-    try {
-      const loadedItems = fs.readFileSync("./Data/Items.json", "utf-8");
-
-      if (loadedItems) {
-        items = JSON.parse(loadedItems);
-      }
-    } catch (error) {
-      console.log("File does not exist , create a new file.");
+    if (fs.existsSync("./data/Items.json")) {
+      const loadedItems = fs.readFileSync("./data/Items.json", "utf8");
+      items = JSON.parse(loadedItems);
+    } else {
+      console.log("File not found, creating a new one.");
     }
-
-    const itemExists = items.find((currentItem) => currentItem.title === title);
-
+    const itemExists = items.find((item) => item.title === title);
     if (itemExists) {
-      console.log(`Item '${title}' already exists`);
+      console.log(`Item '${title}' already exists.`);
       return;
     }
-
-    items.push(newItem);
-    fs.writeFileSync("./Data/Items.json", JSON.stringify(items));
-    console.log(`Item '${title}' added successfully`);
+    items.push({ title, quantity, price });
+    fs.writeFileSync("./data/Items.json", JSON.stringify(items));
+    console.log(`Item '${title}' added successfully.`);
   });
 
 //delete item
@@ -99,9 +83,9 @@ tracker
     });
   });
 
-  //GET ITEM
+//GET ITEM
 
-  tracker
+tracker
   .command("get")
   .description("gets a specific budget item by title")
   .option("-t, --title <value>", "title of the budget item")
@@ -109,21 +93,53 @@ tracker
     const loadedItems = fs.readFileSync("./data/Items.json", "utf8");
     const items = JSON.parse(loadedItems);
 
-   
     if (items.length === 0) {
       console.log("You don't have any items.");
       return;
     }
-    const foundItem = items.find(item => item.title === options.title);
+    const foundItem = items.find((item) => item.title === options.title);
     if (!foundItem) {
       console.log(`Item with title "${options.title}" not found.`);
     } else {
-     
       console.log(`Title: ${foundItem.title}`);
       console.log(`Quantity: ${foundItem.quantity}`);
       console.log(`Price: ${foundItem.price}`);
     }
   });
 
+//UPDATE ITEM
+
+tracker
+  .command("update")
+  .description("updates a specific budget item by title")
+  .option("-t, --title <value>", "title of the budget item")
+  .option("-q, --quantity <value>", "new quantity of the budget item")
+  .option("-p, --price <value>", "new price of the budget item")
+  .action(function (options) {
+    const loadedItems = fs.readFileSync("./data/Items.json", "utf8");
+    const items = JSON.parse(loadedItems);
+    if (items.length === 0) {
+      console.log("You don't have any items.");
+      return;
+    }
+    const foundIndex = items.findIndex((item) => item.title === options.title);
+
+    if (foundIndex === -1) {
+      console.log(`Item with title "${options.title}" not found.`);
+      return;
+    }
+
+    if (options.quantity) {
+      items[foundIndex].quantity = options.quantity;
+    }
+
+    if (options.price) {
+      items[foundIndex].price = options.price;
+    }
+
+    fs.writeFileSync("./data/Items.json", JSON.stringify(items, null, 2));
+
+    console.log(`Item "${options.title}" has been updated.`);
+  });
 
 tracker.parse(process.argv);
